445a7b5cac676400559e962fefeafb49f92bd644
padelhardt <patrickadelhardt@wireless-10-104-17-190.umd.edu>
2024-11-11 11:15:00 -0500

    updated fidelity joblist

--- generate_fidelity_joblist.py
+++ generate_fidelity_joblist.py
@@ -5,14 +5,16 @@ import numpy as np
 filename = "joblist_dmrg.txt"
 #script = "ed_fidelity.py"
 script = "dmrg_spinone_heisenberg_fidelity.py"
-L = 20
-alpha = 10.0
-Ds = np.arange(-1.0,0.5,0.02)
+L = 60
+alpha = 'inf'
+Ds = np.arange(-0.4,0.8,0.02)
+
+#TODO fixme n e! -> common file with other joblist!
 
 # Writing to file
 with open(filename, "w") as file:
     for D in Ds:
-        line = f"python {script} -L {L} -D {D} -a {alpha}\n"
+        line = f"python {script} -L {L} -D {D} -a {alpha} -e 10 \n"
         file.write(line)
 
 print(f"Output written to {filename}")
