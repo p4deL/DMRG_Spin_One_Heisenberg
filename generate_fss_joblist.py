3b50e429ae5938a003d1b1dfebf49185cded9490
padelhardt <patrickadelhardt@wireless-10-104-20-91.umd.edu>
2024-11-25 11:08:32 -0500

    upated fss joblist script to accomodate lambda parameters

--- generate_fss_joblist.py
+++ generate_fss_joblist.py
@@ -12,42 +12,44 @@ if __name__ == "__main__":
     script = "dmrg_spinone_heisenberg_fss.py"
     basename = "joblist_dmrg_fss"
     plotflag = False
+    lambdaflag = True
     alpha = float('inf')
-    Ls = [60, 80, 100, 120, 140, 160, 180, 200]
-    Dcmin = -0.25
-    Dcinf = -0.31 # guess for phase transition point
-    #Dcmax = -0.28
+    Ls = [40, 50, 60]
+    #Ls = [40, 60, 80, 100, 120, 140, 100, 240, 280, 320]
+    xcmin = 0.38
+    xcinf = 0.385  # guess for phase transition point
     koppa = 1.
     nu = 1.
-    #nonuni_pref_Dc = (Dcmin-Dcinf)*Ls[0]**(koppa/nu)  # choose such that it coincides with Dcmin
-    nonuni_pref_range = 4.
+    nonuni_pref_range = 10. # 4.
     # should locate peak for min and max length, then scale appropriately with expected exponent
-    #Dcs = [nonuni_pref_Dc*L**(-koppa/nu) + Dcinf for L in Ls]
-    #Dcs = np.linspace(Dcmin, Dcmax, len(Ls))  # Can also be used if nu=1
-    n_datapoints = 40
+    #nonuni_pref_xc = (xcmin-xcinf)*Ls[0]**(koppa/nu)  # choose such that it coincides with Dcmin
+    #xcs = [nonuni_pref_xc*L**(-koppa/nu) + xcinf for L in Ls]
+    #print(xcs)
+    n_datapoints = 80
     err_tol = 1e-9
-    #alphas = np.reciprocal(np.arange(0.0, 0.82, 0.02))
-    #print(alphas)
     #######################################################
 
     n_exp_min = n_exp = 1
-    #for i, (L, Dc) in enumerate(zip(Ls, Dcs)):
+    #for i, (L, xc) in enumerate(zip(Ls, xcs)):
     for i, L in enumerate(Ls):
         print(f"L={L}")
-        Dmin = Dcinf - nonuni_pref_range*L**(-koppa/nu)
-        Dmax = Dcinf + nonuni_pref_range*L**(-koppa/nu)
-        Ds = np.linspace(Dmin, Dmax, n_datapoints)
-        #print(f"Dc={Dc}")
-        print(f"Dmin={Dmin}, Dmax={Dmax}")
+        xmin = xcinf - nonuni_pref_range*L**(-koppa/nu)
+        xmax = xcinf + nonuni_pref_range*L**(-koppa/nu)
+        xs = np.linspace(xmin, xmax, n_datapoints)
+        if lambdaflag:
+            xs = np.reciprocal(xs)  # convert lambdas into Ds
+
+        #print(f"xc={xc}")
+        print(f"xmin={xmin}, xmax={xmax}")
 
         if math.isinf(alpha):
-            data_io.write_joblist_files(basename, script, L, alpha, Ds, n_exp)
+            data_io.write_joblist_files(basename, script, L, alpha, xs, n_exp)
         else:
             n_exp = utilities.determine_n_exp(n_exp_min=n_exp_min, err_tol=err_tol, L=L, alpha=alpha, plot=plotflag)
             n_exp_min = n_exp
-            data_io.write_joblist_files(basename, script, L, alpha, Ds, n_exp)
+            data_io.write_joblist_files(basename, script, L, alpha, xs, n_exp)
 
-        data_io.write_one_joblist_file(f"{basename}_all_alpha{alpha}.txt", script, L, alpha, Ds, n_exp, append=i)
+        data_io.write_one_joblist_file(f"{basename}_all_alpha{alpha}.txt", script, L, alpha, xs, n_exp, append=i)
 
 
 
